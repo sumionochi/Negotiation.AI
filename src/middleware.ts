f703c9722feba7df795e4415e9d685293eab1c23
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-04-25 20:26:47 +0530

    loosened the middleware security due to vercel issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import {withAuth} from 'next-auth/middleware';
+import { withAuth } from "next-auth/middleware";
 
 export default withAuth;
 
 export const config = {
-  matcher: ['/chat', '/chat/:id*','/register', '/home'],
+  matcher: [],
 };
